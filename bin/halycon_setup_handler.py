169612b87a4b1daa7797b522ae0bfd75449c9e99
Mark Teicher <mark.teicher@gmail.com>
2026-01-07 23:10:26 -0500

    Update halycon_setup_handler.py

--- bin/halycon_setup_handler.py
+++ bin/halycon_setup_handler.py
@@ -2,68 +2,51 @@
 #
 # Halcyon.ai for Splunk App
 # Setup REST handler
-#
-# Handles initial configuration, credential storage, and reloads
 
-import json
 import splunk.admin as admin
 import splunk.entity as entity
 import splunk.rest as rest
 
-
-APP_NAME = "Halcyon_For_Splunk"
+APP_NAME = "halcyon_for_splunk"
 CONF_FILE = "halcyon"
 CONF_STANZA = "api"
-
+CRED_REALM = "halcyon"
 
 class HalcyonSetupHandler(admin.MConfigHandler):
 
     def setup(self):
-        if self.requestedAction == admin.ACTION_CREATE:
-            for arg in [
-                "api_token",
-                "api_base_url",
-                "verify_ssl",
-                "proxy_enabled",
-                "proxy_url",
-                "proxy_username",
-                "proxy_password"
-            ]:
-                self.supportedArgs.addOptArg(arg)
+        for arg in [
+            "halcyon_api_token",
+            "halcyon_api_base_url",
+            "halcyon_verify_ssl",
+            "halcyon_use_proxy",
+            "halcyon_proxy_url",
+            "halcyon_proxy_username",
+            "halcyon_proxy_password"
+        ]:
+            self.supportedArgs.addOptArg(arg)
 
     def handleCreate(self, confInfo):
+        self._handle_write()
 
-        api_token = self.callerArgs.get("api_token", [None])[0]
-        api_base_url = self.callerArgs.get("api_base_url", ["https://api.halcyon.ai"])[0]
-        verify_ssl = self.callerArgs.get("verify_ssl", ["true"])[0]
-        proxy_enabled = self.callerArgs.get("proxy_enabled", ["false"])[0]
-        proxy_url = self.callerArgs.get("proxy_url", [""])[0]
-        proxy_username = self.callerArgs.get("proxy_username", [""])[0]
-        proxy_password = self.callerArgs.get("proxy_password", [""])[0]
+    def handleEdit(self, confInfo):
+        self._handle_write()
+
+    def _handle_write(self):
+
+        api_token = self._get("halcyon_api_token")
+        api_base_url = self._get("halcyon_api_base_url", "https://api.halcyon.ai")
+        verify_ssl = self._get("halcyon_verify_ssl", "true")
+        proxy_enabled = self._get("halcyon_use_proxy", "false")
+        proxy_url = self._get("halcyon_proxy_url")
+        proxy_username = self._get("halcyon_proxy_username")
+        proxy_password = self._get("halcyon_proxy_password")
 
         if not api_token:
-            raise Exception("API token is required")
+            raise admin.ArgValidationException("API token is required")
 
-        self._write_conf(
-            api_base_url,
-            verify_ssl,
-            proxy_enabled,
-            proxy_url,
-            proxy_username
-        )
-
-        self._store_credential(api_token, proxy_password)
-
-    def _write_conf(
-        self,
-        api_base_url,
-        verify_ssl,
-        proxy_enabled,
-        proxy_url,
-        proxy_username
-    ):
         entity.setEntity(
-            "configs/conf-" + CONF_FILE,
+            f"configs/conf-{CONF_FILE}",
             CONF_STANZA,
             {
                 "api_base_url": api_base_url,
@@ -77,27 +60,33 @@ class HalcyonSetupHandler(admin.MConfigHandler):
             namespace=APP_NAME
         )
 
-    def _store_credential(self, api_token, proxy_password):
-
-        if api_token:
-            rest.simpleRequest(
-                "/servicesNS/nobody/{}/storage/passwords".format(APP_NAME),
-                sessionKey=self.getSessionKey(),
-                postargs={
-                    "name": "halcyon_api_token",
-                    "password": api_token
-                }
-            )
+        self._store_secret("api_token", api_token)
 
         if proxy_password:
-            rest.simpleRequest(
-                "/servicesNS/nobody/{}/storage/passwords".format(APP_NAME),
-                sessionKey=self.getSessionKey(),
-                postargs={
-                    "name": "halcyon_proxy_password",
-                    "password": proxy_password
-                }
-            )
+            self._store_secret("proxy_password", proxy_password)
+
+        self._reload_conf()
+
+    def _store_secret(self, name, value):
+        rest.simpleRequest(
+            f"/servicesNS/nobody/{APP_NAME}/storage/passwords",
+            sessionKey=self.getSessionKey(),
+            postargs={
+                "name": name,
+                "password": value,
+                "realm": CRED_REALM
+            }
+        )
+
+    def _reload_conf(self):
+        rest.simpleRequest(
+            "/services/admin/conf-deploy/_reload",
+            sessionKey=self.getSessionKey(),
+            method="POST"
+        )
+
+    def _get(self, key, default=""):
+        return self.callerArgs.get(key, [default])[0]
 
 
 admin.init(HalcyonSetupHandler, admin.CONTEXT_APP_ONLY)
